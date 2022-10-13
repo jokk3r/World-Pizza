e959214b6b05a6a433d3b12170b2b6bd5c8462aa
jokk3r <jokzer43@gmail.com>
2022-10-13 12:47:26 +0200

    close popup window in sort if u click outside

--- src/components/Sort/Sort.js
+++ src/components/Sort/Sort.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState,useRef,useEffect } from "react";
+
 import { useDispatch, useSelector} from 'react-redux'
 import {setSort} from './../../redux/slices/filterSlice'
 
@@ -14,13 +15,25 @@ function Sort(){
   const sort = useSelector(state => state.filter.sort)
 
   const [open, setOpen] = useState(false);
+  const sortRef = useRef();
+  const onClickListItem=(obj)=>{
+    dispatch(setSort(obj)) 
+    setOpen(false);
+  }
+  useEffect(()=>{
+    const handleClickOutside = (event)=>{
+      if(!event.path.includes(sortRef.current)){
+        setOpen(false)
+       }
+    }
+    document.body.addEventListener('click', handleClickOutside)
 
-const onClickListItem=(obj)=>{
-  dispatch(setSort(obj)) 
-  setOpen(false);
-}
+    return()=>{
+      document.body.removeEventListener('click',handleClickOutside)
+    }
+  },[])
     return(
-      <div className="sort">
+      <div ref={sortRef} className="sort">
                 <div className="sort__label">
                   <svg
                     width="10"
